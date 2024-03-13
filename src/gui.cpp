6fac63d165fb90b8c7b757bcd886f00e12d120e5
Ivan Gagis <igagis@gmail.com>
2024-03-13 22:04:48 +0200

    gauge

--- src/gui.cpp
+++ src/gui.cpp
@@ -14,64 +14,80 @@ utki::shared_ref<ruis::widget> carcockpit::make_root_layout(utki::shared_ref<rui
     namespace m = ruis::make;
 	using ruis::lp;
 
-	return
-		// clang-format off
-		m::container(
-			c,
-			{
-				.container_params = {
-					.layout = ruis::layout::column
-				}
-			},
-			{
-                utki::dynamic_reference_cast<ruis::widget>(
-                    utki::make_shared<ruis::horizontal_scroll_bar>(
-                        c,
-                        tml::read(R"(
-                            lp{dx{fill}}
-                        )")
-                    )
-                ),
-                m::container(
+	// clang-format off
+	auto w = m::container(
+        c,
+        {
+            .container_params = {
+                .layout = ruis::layout::column
+            }
+        },
+        {
+            // TODO: should be slider, not scroll bar, band is not needed
+            utki::dynamic_reference_cast<ruis::widget>(
+                utki::make_shared<ruis::horizontal_scroll_bar>(
                     c,
-                    {
-                        .widget_params = {
-                            .lp = {
-                                .dims = {lp::fill, 0},
-                                .weight = 1
-                            }
-                        },
-                        .container_params = {
-                            .layout = ruis::layout::pile
+                    tml::read(R"(
+                        id{gauge_slider}
+                        lp{dx{fill}}
+                    )")
+                )
+            ),
+            m::container(
+                c,
+                {
+                    .widget_params = {
+                        .lp = {
+                            .dims = {lp::fill, 0},
+                            .weight = 1
                         }
                     },
-                    {
-                        m::image(
-                            c,
-                            {
-                                .widget_params = {
-                                    .lp = {
-                                        .dims = {lp::fill, lp::fill}
-                                    }
-                                },
-                                .image_params = {
-                                    .img = c.get().loader.load<ruis::res::image>("ruis_img_gauge_scale")
-                                }
-                            }
-                        ),
-                        utki::dynamic_reference_cast<ruis::widget>(
-                            utki::make_shared<ruis::gauge>(c,
-                            tml::read(R"(
-                                id{gauge}
-                                lp{dx{fill} dy{fill}}
-                                armFraction{0.75}
-                                startAngle{225}
-                                endAngle{-45}
-                            )"))
-                        )
+                    .container_params = {
+                        .layout = ruis::layout::pile
                     }
-                )
-            }
-		);
+                },
+                {
+                    m::image(
+                        c,
+                        {
+                            .widget_params = {
+                                .lp = {
+                                    .dims = {lp::fill, lp::fill}
+                                }
+                            },
+                            .image_params = {
+                                .img = c.get().loader.load<ruis::res::image>("ruis_img_gauge_scale")
+                            }
+                        }
+                    ),
+                    utki::dynamic_reference_cast<ruis::widget>(
+                        utki::make_shared<ruis::gauge>(c,
+                        tml::read(R"(
+                            id{gauge}
+                            lp{dx{fill} dy{fill}}
+                            armFraction{0.75}
+                            startAngle{225}
+                            endAngle{-45}
+                        )"))
+                    )
+                }
+            )
+        }
+    );
 	// clang-format on
+
+    auto gauge = w.get().try_get_widget_as<ruis::gauge>("gauge");
+    ASSERT(gauge)
+    auto weakGauge = utki::make_weak(gauge);
+    
+    auto slider = w.get().try_get_widget_as<ruis::fraction_widget>("gauge_slider");
+    ASSERT(slider)
+
+    slider->fraction_change_handler = [weakGauge](ruis::fraction_widget& s){
+        if(auto g = weakGauge.lock()){
+            g->set_fraction(s.fraction());
+        }
+    };
+
+    return w;
 }
