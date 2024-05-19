f110e4225f08a17ebe9c6b263780fb61400fcfec
Ivan Gagis <igagis@gmail.com>
2024-05-20 00:05:46 +0300

    use deserializator

--- src/ruis/render/scene/scene.cpp
+++ src/ruis/render/scene/scene.cpp
@@ -22,6 +22,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "scene.hpp"
 
 #include <jsondom/dom.hpp>
+#include <utki/deserializator.hpp>
 #include <utki/string.hpp>
 #include <utki/util.hpp>
 
@@ -33,65 +34,39 @@ utki::shared_ref<scene> ruis::render::read_gltf(const papki::file& fi, ruis::ren
 {
 	auto gltf = fi.load();
 
-	auto p = utki::make_span(gltf);
+	utki::deserializator d(gltf);
 
 	constexpr auto gltf_header_size = 4;
-
-	if (p.size() < gltf_header_size) {
-		throw std::invalid_argument("read_gltf(): file too short for glTF header");
-	}
-
-	ASSERT(p.size() >= gltf_header_size)
-	if (auto header_sv = utki::make_string_view(p.subspan(0, gltf_header_size)); header_sv != "glTF"sv) {
+	if (auto header_sv = d.read_string(gltf_header_size); header_sv != "glTF"sv) {
 		throw std::invalid_argument(utki::cat("read_gltf(): file header is not 'glTF': ", header_sv));
 	}
 
-	p = p.subspan(gltf_header_size);
-
-	constexpr auto num_expected_fields_after_header = 4;
-	if (auto es = num_expected_fields_after_header * sizeof(uint32_t); p.size() < es) {
-		throw std::invalid_argument(utki::cat("read_gltf(): glTF file is too short: ", es));
-	}
-
-	auto version = utki::deserialize32le(p.data());
-	p = p.subspan(sizeof(uint32_t));
-
+	auto version = d.read_uint32_le();
 	constexpr auto expected_gltf_version = 2;
 	if (version != expected_gltf_version) {
 		throw std::invalid_argument(utki::cat("read_gltf(): glTF file version is not as expected: ", version));
 	}
 
-	auto gltf_length = utki::deserialize32le(p.data());
-	p = p.subspan(sizeof(uint32_t));
-
+	auto gltf_length = d.read_uint32_le();
 	if (gltf_length != gltf.size()) {
 		throw std::invalid_argument(
 			utki::cat("read_gltf(): glTF file size ", gltf.size(), " does not match length ", gltf_length)
 		);
 	}
 
-	auto chunk_length = utki::deserialize32le(p.data());
-	p = p.subspan(sizeof(uint32_t));
-
+	auto chunk_length = d.read_uint32_le();
 	if (chunk_length == 0) {
 		throw std::invalid_argument("read_gltf(): chunk length = 0");
 	}
 
 	constexpr auto chunk_type_length = 4;
-	auto chunk_type = utki::make_string_view(p.subspan(0, chunk_type_length));
-	p = p.subspan(chunk_type_length);
-
-	if (chunk_type != "JSON"sv) {
+	if (auto chunk_type = d.read_string(chunk_type_length); chunk_type != "JSON"sv) {
 		throw std::invalid_argument(
 			utki::cat("read_gltf(): unexpected first chunk type: ", chunk_type, ", expected JSON")
 		);
 	}
 
-	if (p.size() < chunk_length) {
-		throw std::invalid_argument("read_gltf(): glTF file too short");
-	}
-
-	auto json_span = p.subspan(0, chunk_length);
+	auto json_span = d.read_span(chunk_length);
 	auto json = jsondom::read(json_span);
 
 	ASSERT(json.is_object())
@@ -99,8 +74,6 @@ utki::shared_ref<scene> ruis::render::read_gltf(const papki::file& fi, ruis::ren
 	for (const auto& kv : json.object()) {
 		std::cout << "key = " << kv.first << ", value type = " << unsigned(kv.second.get_type()) << std::endl;
 	}
-
-	p = p.subspan(chunk_length);
 
 	// read binary chunk
 	{
