cb961033447045964e9c5f095cf5e41ed570bc3e
Ivan Gagis <igagis@gmail.com>
2025-03-16 14:43:21 +0200

    lint only in asan config

--- config/asan.mk
+++ config/asan.mk
@@ -1,6 +1,7 @@
 include $(config_dir)rel.mk
 
-this_lint_cmd :=
+# this_lint_cmd :=
+this_lint_cmd = $(prorab_lint_cmd_clang_tidy)
 
 this_cxxflags += -fsanitize=address
 this_ldflags += -fsanitize=address
